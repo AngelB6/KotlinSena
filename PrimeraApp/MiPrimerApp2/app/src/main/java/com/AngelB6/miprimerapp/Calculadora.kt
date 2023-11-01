8aed7bc10a52f830584ccf08f119a1fa241ccd53
AngelB6 <131718915+AngelB6@users.noreply.github.com>
2023-11-01 11:55:50 -0500

    Commit Creación De Proyecto
    Proyecto móvil cargado en carpeta GitHub.

---
+++ PrimeraApp/MiPrimerApp2/app/src/main/java/com/AngelB6/miprimerapp/Calculadora.kt
@@ -0,0 +1,27 @@
+package com.AngelB6.miprimerapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class Calculadora : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculadora)
+        val num1 = findViewById<EditText>(R.id.editTextTextNum1)
+        val num2 = findViewById<EditText>(R.id.editTextTextNum2)
+        val textResultado = findViewById<TextView>(R.id.textViewResultado)
+        val botonSuma = findViewById<Button>(R.id.buttonSumar)
+
+        botonSuma.setOnClickListener(){
+            var reaultado = num1.text.toString().toDouble() + num2.text.toString().toDouble()
+//            textResultado.text = "Resultado"
+            textResultado.setText("Resultado: ${reaultado.toString()}")
+            val intento = Intent(this, Calculadora::class.java)
+            startActivity(intento)
+        }
+    }
+}
---
+++ PrimeraApp/MiPrimerApp2/app/src/main/java/com/AngelB6/miprimerapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.AngelB6.miprimerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val nombre = findViewById<EditText>(R.id.editTextNombre)
+        val botonSaludar = findViewById<Button>(R.id.Saludar)
+        botonSaludar.setOnClickListener{
+            var nombreRecogido = nombre.text.toString()
+            Toast.makeText(this, "Bienvenido ${nombreRecogido} a mi APP!", Toast.LENGTH_LONG).show()
+        }
+    }
+}
